874aa6572414d0cb94f7a09c4986fce93e8b1170
Chanwoongs <tkfkdcksdnd@daum.net>
2022-09-28 02:14:06 +0900

    Print 함수에 출력할 인자를 다양한 자료형으로 오버로딩

--- C++/Cpp_Project/Source/Cpp_Project/Utilities/CLog.cpp
+++ C++/Cpp_Project/Source/Cpp_Project/Utilities/CLog.cpp
@@ -5,3 +5,23 @@ void CLog::Print(int32 InValue, int32 InKey, float Duration, FColor InColor)
 {
 	GEngine->AddOnScreenDebugMessage(InKey, Duration, InColor, FString::FromInt(InValue));
 }
+
+void CLog::Print(float InValue, int32 InKey, float Duration, FColor InColor)
+{
+	GEngine->AddOnScreenDebugMessage(InKey, Duration, InColor, FString::SanitizeFloat(InValue));
+}
+
+void CLog::Print(const FString& InValue, int32 InKey, float Duration, FColor InColor)
+{
+	GEngine->AddOnScreenDebugMessage(InKey, Duration, InColor, InValue);
+}
+
+void CLog::Print(const FVector& InValue, int32 InKey, float Duration, FColor InColor)
+{
+	GEngine->AddOnScreenDebugMessage(InKey, Duration, InColor, InValue.ToString());
+}
+
+void CLog::Print(const FRotator& InValue, int32 InKey, float Duration, FColor InColor)
+{
+	GEngine->AddOnScreenDebugMessage(InKey, Duration, InColor, InValue.ToString());
+}
--- C++/Cpp_Project/Source/Cpp_Project/Utilities/CLog.h
+++ C++/Cpp_Project/Source/Cpp_Project/Utilities/CLog.h
@@ -6,4 +6,8 @@ class CPP_PROJECT_API CLog
 {
 public:
 	static void Print(int32 InValue, int32 InKey = -1, float Duration = 10.0f, FColor InColor = FColor::Blue);
+	static void Print(float InValue, int32 InKey = -1, float Duration = 10.0f, FColor InColor = FColor::Blue);
+	static void Print(const FString& InValue, int32 InKey = -1, float Duration = 10.0f, FColor InColor = FColor::Blue);
+	static void Print(const FVector& InValue, int32 InKey = -1, float Duration = 10.0f, FColor InColor = FColor::Blue);
+	static void Print(const FRotator& InValue, int32 InKey = -1, float Duration = 10.0f, FColor InColor = FColor::Blue);
 };
