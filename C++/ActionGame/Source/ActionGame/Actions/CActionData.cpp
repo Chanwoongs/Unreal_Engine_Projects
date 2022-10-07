dcd2c9ce66a1348b5e1beae83c856cb3ad2d7800
Chanwoongs <tkfkdcksdnd@daum.net>
2022-10-07 21:53:11 +0900

    장비들을 관리하는 DataAsset

--- C++/ActionGame/Source/ActionGame/Actions/CActionData.cpp
+++ C++/ActionGame/Source/ActionGame/Actions/CActionData.cpp
@@ -1,3 +1,22 @@
 #include "CActionData.h"
 #include "Global.h"
+#include "CEquipment.h"
+#include "GameFramework/Character.h"
+#include "Components/SkeletalMeshComponent.h"
 
+void UCActionData::BeginPlay(class ACharacter* InOwnerCharacter)
+{
+	FTransform transform;
+
+	// Equipment
+	// 기존 SpawnActor 사용시에는 등장과 동시에 BeginPlay가 호출된다
+	// 객체만 생성한 후 값을 세팅하고 등장을 시키는 방법을 사용할 것이다
+	// 이걸 SpawnActorDeffered 라고 함
+	{
+		// 객체 생성만 완료된 것이고, 등장하진 않았다
+		Equipment = InOwnerCharacter->GetWorld()->SpawnActorDeferred<ACEquipment>(EquipmentClass, transform, InOwnerCharacter);
+		Equipment->AttachToComponent(InOwnerCharacter->GetMesh(), FAttachmentTransformRules(EAttachmentRule::KeepRelative, true));
+		Equipment->SetData(EquipmentData);
+		UGameplayStatics::FinishSpawningActor(Equipment, transform); // 등장 확정
+	}
+}
--- C++/ActionGame/Source/ActionGame/Actions/CActionData.h
+++ C++/ActionGame/Source/ActionGame/Actions/CActionData.h
@@ -27,6 +27,18 @@ UCLASS()
 class ACTIONGAME_API UCActionData : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+		TSubclassOf<class ACEquipment> EquipmentClass;
+
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+		FEquipmentData EquipmentData;
 	
-		
+public:
+	void BeginPlay(class ACharacter* InOwnerCharacter);
+
+private:
+	class ACEquipment* Equipment;
 };
