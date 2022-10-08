4da1ab709beb180828a308f2d3a1af648b850e4e
Chanwoongs <tkfkdcksdnd@daum.net>
2022-10-08 20:05:49 +0900

    Enemy기본 세팅

--- C++/ActionGame/Source/ActionGame/Characters/CEnemy.cpp
+++ C++/ActionGame/Source/ActionGame/Characters/CEnemy.cpp
@@ -1,10 +1,30 @@
 #include "CEnemy.h"
 #include "Global.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Animation/AnimInstance.h"
+#include "Components/SkeletalMeshComponent.h"
+
+#include "Materials/MaterialInstanceDynamic.h"
+#include "Materials/MaterialInstanceConstant.h"
+
+
 ACEnemy::ACEnemy()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetMesh()->SetRelativeLocation(FVector(0, 0, -90));
+	GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
+
+	USkeletalMesh* mesh;
+	CHelpers::GetAsset<USkeletalMesh>(&mesh, "SkeletalMesh'/Game/Character/Mesh/SK_Mannequin.SK_Mannequin'");
+	GetMesh()->SetSkeletalMesh(mesh);
+
+	TSubclassOf<UAnimInstance> animInstance;
+	CHelpers::GetClass<UAnimInstance>(&animInstance, "AnimBlueprint'/Game/Enemies/ABP_CEnemy.ABP_CEnemy_C'");
+	GetMesh()->SetAnimInstanceClass(animInstance);
+
+	GetCharacterMovement()->RotationRate = FRotator(0, 720, 0);
 }
 
 void ACEnemy::BeginPlay()
