c88b9ff390820e3b52e5df6b2a943d9c36d218be
Chanwoongs <tkfkdcksdnd@daum.net>
2022-10-08 17:24:21 +0900

    Action 시작을 알리는 Notify 추가

--- C++/ActionGame/Source/ActionGame/Notifies/CAnimNotifyState_Equip.h
+++ C++/ActionGame/Source/ActionGame/Notifies/CAnimNotifyState_Equip.h
@@ -8,6 +8,7 @@ class ACTIONGAME_API UCAnimNotifyState_Equip : public UAnimNotifyState
 {
 	GENERATED_BODY()
 	
+public:
 	FString GetNotifyName_Implementation() const override;
 
 	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration) override;
---
+++ C++/ActionGame/Source/ActionGame/Notifies/CAnimNotify_BeginAction.cpp
@@ -0,0 +1,28 @@
+#include "CAnimNotify_BeginAction.h"
+#include "Global.h"
+#include "Characters/CPlayer.h"
+#include "Actions/CActionData.h"
+#include "Actions/CDoAction.h"
+#include "Components/CActionComponent.h"
+
+FString UCAnimNotify_BeginAction::GetNotifyName_Implementation()const
+{
+	return "BeginAction";
+}
+
+void UCAnimNotify_BeginAction::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::Notify(MeshComp, Animation);
+	// Notify에서 항상 체크
+	CheckNull(MeshComp);
+	CheckNull(MeshComp->GetOwner());
+
+	UCActionComponent* action = CHelpers::GetComponent<UCActionComponent>(MeshComp->GetOwner());
+	CheckNull(action);
+
+	action->GetCurrent()->GetDoAction()->Begin_Action();
+}
+
+
+
+
