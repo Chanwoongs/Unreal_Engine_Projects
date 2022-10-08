dc07faea8ce2544aed595ca5c4820bdb715f4ff9
Chanwoongs <tkfkdcksdnd@daum.net>
2022-10-08 19:02:23 +0900

    Character들의 공통 기능을 관리할 Interface 생성

---
+++ C++/ActionGame/Source/ActionGame/Characters/ICharacter.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ICharacter.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UICharacter : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ACTIONGAME_API IICharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Mesh 색 변경하는 함수
+	virtual void ChangeColor(FLinearColor InColor) = 0;
+};
