3198550bdff3cc37d20ef432ceebfa669bb6341c
Chanwoongs <tkfkdcksdnd@daum.net>
2022-10-08 17:25:03 +0900

    Combo구간을 입력받는 NotifyState 추가 / Combo 구현

--- C++/ActionGame/Source/ActionGame/Actions/CDoAction_Melee.cpp
+++ C++/ActionGame/Source/ActionGame/Actions/CDoAction_Melee.cpp
@@ -32,12 +32,30 @@ void ACDoAction_Melee::DoAction()
 void ACDoAction_Melee::Begin_Action()
 {
 	Super::Begin_Action();
+
+	CheckFalse(bExist);
+	bExist = false;
+
+	// StopAnimMontage : 현재 플레이 중인 모든 몽타주 중지, 이후에 Fist의 마지막 동작을 끊어서 사용하기 위하여
+	OwnerCharacter->StopAnimMontage(); 
+
+	Index++;
+
+	const FDoActionData& data = Datas[Index];
+	OwnerCharacter->PlayAnimMontage(data.AnimMontage, data.PlayRatio, data.StartSection);
+
+	data.bCanMove ? Status->SetMove() : Status->SetStop();
 }
 
 void ACDoAction_Melee::End_Action()
 {
 	Super::End_Action();
 
+	const FDoActionData& data = Datas[Index];
+	OwnerCharacter->StopAnimMontage(data.AnimMontage); // 현재 동작만 중단
+
 	State->SetIdleMode();
 	Status->SetMove();
+
+	Index = 0;
 }
