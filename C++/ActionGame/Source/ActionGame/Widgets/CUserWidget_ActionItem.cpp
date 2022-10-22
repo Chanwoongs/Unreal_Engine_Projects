3849c46754045946be328b611b0a103653523c20
Chanwoongs <tkfkdcksdnd@daum.net>
2022-10-22 16:38:34 +0900

    버튼을 클릭하여 ActionItem들을 선택할 수있는 Widget 클래스 생성

---
+++ C++/ActionGame/Source/ActionGame/Widgets/CUserWidget_ActionItem.cpp
@@ -0,0 +1,14 @@
+#include "CUserWidget_ActionItem.h"
+#include "Global.h"
+
+void UCUserWidget_ActionItem::Click()
+{
+}
+
+void UCUserWidget_ActionItem::Hover()
+{
+}
+
+void UCUserWidget_ActionItem::Unhover()
+{
+}
---
+++ C++/ActionGame/Source/ActionGame/Widgets/CUserWidget_ActionItem.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CUserWidget_ActionItem.generated.h"
+
+UCLASS()
+class ACTIONGAME_API UCUserWidget_ActionItem : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	UFUNCTION(BlueprintCallable)
+		void Click();
+
+	UFUNCTION(BlueprintCallable)
+		void Hover();
+			
+	UFUNCTION(BlueprintCallable)
+		void Unhover();
+};
