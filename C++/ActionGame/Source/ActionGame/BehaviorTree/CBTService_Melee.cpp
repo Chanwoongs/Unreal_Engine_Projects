db1690f10004d06abfe8d6720cf6b7762043adaf
Chanwoongs <tkfkdcksdnd@daum.net>
2022-11-02 03:15:03 +0900

    공격에 대한 Behavior 상태 정의

---
+++ C++/ActionGame/Source/ActionGame/BehaviorTree/CBTService_Melee.cpp
@@ -0,0 +1,65 @@
+#include "CBTService_Melee.h"
+#include "Global.h"
+#include "Characters/CPlayer.h"
+#include "Characters/CAIController.h"
+#include "Characters/CEnemy_AI.h"
+#include "Components/CBehaviorComponent.h"
+#include "Components/CstateComponent.h"
+
+UCBTService_Melee::UCBTService_Melee()
+{
+	NodeName = "Melee"; // 노드 이름 세팅
+}
+
+void UCBTService_Melee::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	ACAIController* controller = Cast<ACAIController>(OwnerComp.GetOwner());
+	UCBehaviorComponent* behavior = CHelpers::GetComponent<UCBehaviorComponent>(controller);
+
+	ACEnemy_AI* ai = Cast<ACEnemy_AI>(controller->GetPawn());
+	UCStateComponent* state = CHelpers::GetComponent<UCStateComponent>(ai);
+
+	if (state->IsHittedMode())
+	{
+		behavior->SetHittedMode();
+
+		return;
+	}
+
+	ACPlayer* target = behavior->GetTargetPlayer();
+	if (target == NULL)
+	{
+		// Patrol Mode
+
+		// Wait Mode
+		behavior->SetWaitMode();
+		return;
+	}
+	else
+	{
+		UCStateComponent* targetState = CHelpers::GetComponent<UCStateComponent>(target);
+		if (targetState->IsDeadMode())
+		{
+			behavior->SetWaitMode();
+
+			return;
+		}
+	}
+
+	float distance = ai->GetDistanceTo(target);
+	if (distance < controller->GetMeleeActionRange())
+	{
+		behavior->SetActionMode();
+
+		return;
+	}
+
+	if (distance < controller->GetSightRadius())
+	{
+		behavior->SetApproachMode();
+
+		return;
+	}
+}
--- C++/ActionGame/Source/ActionGame/BehaviorTree/CBTService_Melee.h
+++ C++/ActionGame/Source/ActionGame/BehaviorTree/CBTService_Melee.h
@@ -1,17 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTService.h"
 #include "CBTService_Melee.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class ACTIONGAME_API UCBTService_Melee : public UBTService
 {
 	GENERATED_BODY()
 	
+public:
+	UCBTService_Melee();
+
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
 };
