be25ba72078c9e7ee8ebbbd6bc96905f6aaebb7e
Chanwoongs <tkfkdcksdnd@daum.net>
2022-11-01 22:28:23 +0900

    AIController의 기본적인 정보 세팅 / 감지, 공격 범위 디버깅 세팅 / 적과 아군을 인지

--- C++/ActionGame/Source/ActionGame/Characters/CAIController.cpp
+++ C++/ActionGame/Source/ActionGame/Characters/CAIController.cpp
@@ -1,5 +1,12 @@
 #include "CAIController.h"
 #include "Global.h"
+#include "CEnemy_AI.h"
+#include "CPlayer.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Components/CBehaviorComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
 ACAIController::ACAIController()
 {
@@ -7,5 +14,87 @@ ACAIController::ACAIController()
 	
 	CHelpers::CreateActorComponent<UBlackboardComponent>(this, &Blackboard, "Blackboard");
 	CHelpers::CreateActorComponent<UAIPerceptionComponent>(this, &Perception, "Perception");
+	CHelpers::CreateActorComponent<UCBehaviorComponent>(this, &Behavior, "Behavior");
 
+	// CreateDefaultSubobject : 객체를 생성자에서 동적으로 할당하기 위해 사용, Subobject : 동적 할당된 개체
+	// BeginPlay 이후엔 NewObject 사용
+	Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("Sight");
+	Sight->SightRadius = 600;
+	Sight->LoseSightRadius = 800;
+	Sight->PeripheralVisionAngleDegrees = 90;
+	Sight->SetMaxAge(2);
+
+	Sight->DetectionByAffiliation.bDetectEnemies = true; // 감지할 관계를 설정
+	Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	// TeamID를 두고 ID로 구별
+
+	Perception->ConfigureSense(*Sight);
+	Perception->SetDominantSense(*Sight->GetSenseImplementation()); // 우선 순위
+
+}
+
+float ACAIController::GetSightRadius()
+{
+	return Sight->SightRadius;
+}
+
+void ACAIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	OwnerEnemy = Cast<ACEnemy_AI>(InPawn);
+	SetGenericTeamId(OwnerEnemy->GetTeamID()); // 아군이냐 적군이냐 중립(255)
+
+	Perception->OnPerceptionUpdated.AddDynamic(this, &ACAIController::OnPerceptionUpdated);
+
+	UseBlackboard(OwnerEnemy->GetBehaviorTree()->BlackboardAsset, Blackboard); // Blackboard 할당
+	Behavior->SetBlackboard(Blackboard); // BehaviorTree에 Blackboard 등록
+
+	RunBehaviorTree(OwnerEnemy->GetBehaviorTree());
+}
+
+
+void ACAIController::OnUnPossess()
+{
+	Super::OnUnPossess();
+
+	Perception->OnPerceptionUpdated.Clear();
+}
+
+void ACAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	CheckFalse(bDrawDebug);
+
+	FVector center = OwnerEnemy->GetActorLocation();
+	center.Z -= AdjustCircleHeight;
+	DrawDebugCircle(GetWorld(), center, Sight->SightRadius, 300, FColor::Green, false, -1, 0, 0, 
+		FVector::RightVector, FVector::ForwardVector); // 원을 그릴 회전 방향, 앞에서 오른쪽 시계 방향으로 그린다.
+	DrawDebugCircle(GetWorld(), center, MeleeActionRange, 300, FColor::Red, false, -1, 0, 0,
+		FVector::RightVector, FVector::ForwardVector);
+}
+
+void ACAIController::OnPerceptionUpdated(const TArray<AActor*>& UpdateActors)
+{
+	TArray<AActor*> actors;
+
+	Perception->GetCurrentlyPerceivedActors(NULL, actors);
+
+	ACPlayer* player = NULL;
+	for (AActor* actor : actors)
+	{
+		player = Cast<ACPlayer>(actor);
+
+		if (!!player)
+			break;
+	}
+
+	Blackboard->SetValueAsObject("Player", player);
 }
--- C++/ActionGame/Source/ActionGame/Characters/CAIController.h
+++ C++/ActionGame/Source/ActionGame/Characters/CAIController.h
@@ -11,7 +11,7 @@ class ACTIONGAME_API ACAIController : public AAIController
 	
 private:
 	UPROPERTY(EditAnywhere)
-		float BehaviorRange = 150; // AI의 공격 범위
+		float MeleeActionRange = 150; // AI의 근거리 공격 범위
 
 	UPROPERTY(EditAnywhere)
 		bool bDrawDebug = true;
@@ -23,13 +23,27 @@ private:
 	UPROPERTY(VisibleDefaultsOnly)
 		class UAIPerceptionComponent* Perception; // 감지 컴포넌트
 
+	UPROPERTY(VisibleDefaultsOnly)
+		class UCBehaviorComponent* Behavior;
+
 public:
-	FORCEINLINE float GetBehaviorRange() { return BehaviorRange; }
+	FORCEINLINE float GetMeleeActionRange() { return MeleeActionRange; }
 
 public:
 	ACAIController();
 
+	float GetSightRadius();
 	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void OnPossess(APawn* InPawn) override; // 빙의될 때 호출되는 함수
+	virtual void OnUnPossess() override; // 빙의가 해제될 때 호출되는 함수
+
+private:
+	UFUNCTION()
+		void OnPerceptionUpdated(const TArray<AActor*>& UpdateActors);
 
 private:
 	class ACEnemy_AI* OwnerEnemy;
