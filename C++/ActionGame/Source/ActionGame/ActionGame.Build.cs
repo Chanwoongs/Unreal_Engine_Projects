50336bf2b6b91e37cf420fcdb1bcfa3de962112b
Chanwoongs <tkfkdcksdnd@daum.net>
2022-11-02 02:21:02 +0900

    AI 사용을 위한 모듈 추가 (*중요)

--- C++/ActionGame/Source/ActionGame/ActionGame.Build.cs
+++ C++/ActionGame/Source/ActionGame/ActionGame.Build.cs
@@ -7,8 +7,8 @@ public class ActionGame : ModuleRules
 	public ActionGame(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks", "HeadMountedDisplay" });
 
 		PublicIncludePaths.Add(ModuleDirectory);
 	}
