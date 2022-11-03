fdf75dab1d3ffa7ca8cdf50c3839cef25a7e1a35
Chanwoongs <tkfkdcksdnd@daum.net>
2022-11-04 01:57:34 +0900

    Path의 Spline Point를 탐색하여 Index를 Update하며 목표 위치를 변경하는 컴포넌트

---
+++ C++/ActionGame/Source/ActionGame/Components/CPatrolComponent.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CPatrolComponent.generated.h"
+
+
+UCLASS( ClassGroup=(GameProject), meta=(BlueprintSpawnableComponent) )
+class ACTIONGAME_API UCPatrolComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere)
+		class ACPatrolPath* Path;
+
+	UPROPERTY(EditAnywhere)
+		int32 Index;
+
+	UPROPERTY(EditAnywhere)
+		bool bReverse;
+
+	UPROPERTY(EditAnywhere)
+		float AcceptanceRadius = 50;
+
+public:
+	FORCEINLINE bool IsValid() { return Path != NULL; }
+
+public:	
+	UCPatrolComponent();
+
+	bool GetMoveTo(FVector& OutLocation, float& OutAcceptanceRadius);
+	void UpdateNextIndex();
+
+protected:
+	virtual void BeginPlay() override;
+
+		
+};
