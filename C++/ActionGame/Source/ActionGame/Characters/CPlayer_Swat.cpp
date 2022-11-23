8248faaf9401af67e59f5d7e780e3c9a79c7887c
Chanwoongs <tkfkdcksdnd@daum.net>
2022-11-23 21:56:07 +0900

    Shooting Game의 Swat Character 생성

---
+++ C++/ActionGame/Source/ActionGame/Characters/CPlayer_Swat.cpp
@@ -0,0 +1,200 @@
+#include "CPlayer_Swat.h"
+#include "Global.h"
+#include "Actions/CDoAction_Rifle.h"
+#include "CAnimInstance.h"
+#include "GameFrameWork/SpringArmComponent.h"
+#include "GameFrameWork/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CActionComponent.h"
+#include "Components/CStateComponent.h"
+#include "Actions/CAction.h"
+#include "Actions/CEquipment.h"
+#include "Actions/CDoAction_Rifle.h"
+#include "Components/CapsuleComponent.h"
+#include "Materials/MaterialInstanceConstant.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "Widgets/CUserWidget_Crosshair.h"
+
+
+ACPlayer_Swat::ACPlayer_Swat() // 기본 값은 C++로 할당을 한다. 수정은 블프에서 가능하다.
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 카메라 기본 세팅
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->MaxWalkSpeed = 400.0f;
+
+	// 기본 메쉬 적용
+	USkeletalMesh* mesh;
+	CHelpers::GetAsset<USkeletalMesh>(&mesh, "SkeletalMesh'/Game/Character/Swat/Model/Swat.Swat'");
+	GetMesh()->SetSkeletalMesh(mesh);
+	GetMesh()->SetRelativeLocation(FVector(0, 0, -90));
+	GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
+
+	// animation instance class를 가져와 mesh에 할당하기
+	TSubclassOf<UAnimInstance> animInstance;
+	CHelpers::GetClass<UAnimInstance>(&animInstance, "AnimBlueprint'/Game/Character/Swat/ABP_Swat.ABP_Swat_C'");
+	GetMesh()->SetAnimInstanceClass(animInstance);
+
+	// SpringArm 기본 세팅
+	SpringArm->SetRelativeLocation(FVector(0, 0, 60));
+	SpringArm->TargetArmLength = 200.0f;
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->SocketOffset = FVector(0, 120, 0);
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->MaxWalkSpeed = 400.0f;
+
+	CHelpers::GetClass<UCUserWidget_Crosshair>(&CrosshairClass, "WidgetBlueprint'/Game/Widgets/WB_Crosshair.WB_Crosshair_C'");
+}
+
+void ACPlayer_Swat::GetLocationAndDirection(FVector& OutStart, FVector& OutEnd, FVector& OutDirection)
+{
+	OutDirection = Camera->GetForwardVector();
+
+	FTransform transform = Camera->GetComponentToWorld(); // World 위치 Return
+	FVector cameraLocation = transform.GetLocation();
+	OutStart = cameraLocation + OutDirection;
+
+	// 탄착군 형성
+	FVector conDirection = UKismetMathLibrary::RandomUnitVectorInEllipticalConeInDegrees(OutDirection, 0.2f, 0.3f);
+	conDirection *= 3000.0f; // 사거리
+
+	OutEnd = cameraLocation + conDirection;
+}
+
+void ACPlayer_Swat::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UMaterialInstanceConstant* bodyMaterial;
+	CHelpers::GetAssetDynamic<UMaterialInstanceConstant>(&bodyMaterial, "MaterialInstanceConstant'/Game/Character/Swat/Model/Soldier_body_Inst.Soldier_body_Inst'");
+
+	UMaterialInstanceConstant* headMaterial;
+	CHelpers::GetAssetDynamic<UMaterialInstanceConstant>(&headMaterial, "MaterialInstanceConstant'/Game/Character/Swat/Model/Soldier_body_Inst.Soldier_body_Inst'");
+
+	UMaterialInstanceConstant* faceMaterial;
+	CHelpers::GetAssetDynamic<UMaterialInstanceConstant>(&faceMaterial, "MaterialInstanceConstant'/Game/Character/Swat/Model/Soldier_head_Inst.Soldier_head_Inst'");
+
+	//다이내믹 매터리얼 생성
+	BodyMaterial = UMaterialInstanceDynamic::Create(bodyMaterial, this);
+	HeadMaterial = UMaterialInstanceDynamic::Create(headMaterial, this);
+	FaceMaterial = UMaterialInstanceDynamic::Create(faceMaterial, this);
+
+	GetMesh()->SetMaterial(0, BodyMaterial);
+	GetMesh()->SetMaterial(1, HeadMaterial);
+	GetMesh()->SetMaterial(2, FaceMaterial);
+
+	// Crosshair
+	Crosshair = CreateWidget<UCUserWidget_Crosshair, APlayerController>(GetController<APlayerController>(), CrosshairClass);
+	Crosshair->AddToViewport();
+	Crosshair->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void ACPlayer_Swat::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACPlayer_Swat::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Running", EInputEvent::IE_Pressed, this, &ACPlayer_Swat::OnRunning);
+	PlayerInputComponent->BindAction("Running", EInputEvent::IE_Released, this, &ACPlayer_Swat::OffRunning);
+
+	PlayerInputComponent->BindAction("Rifle", EInputEvent::IE_Pressed, this, &ACPlayer_Swat::OnRifle);
+
+	PlayerInputComponent->BindAction("Aim", EInputEvent::IE_Pressed, this, &ACPlayer_Swat::OnAim);
+	PlayerInputComponent->BindAction("Aim", EInputEvent::IE_Released, this, &ACPlayer_Swat::OffAim);
+
+	PlayerInputComponent->BindAction("Action", EInputEvent::IE_Released, this, &ACPlayer_Swat::OffFire);
+}
+
+// 달리기 액션 입력
+void ACPlayer_Swat::OnRunning()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+}
+void ACPlayer_Swat::OffRunning()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 400.0f;
+}
+
+void ACPlayer_Swat::OnRifle()
+{
+	CheckFalse(State->IsIdleMode());
+
+	Action->SetRifleMode();
+}
+
+void ACPlayer_Swat::ChangeColor(FLinearColor InColor)
+{
+	BodyMaterial->SetVectorParameterValue("BodyColor", InColor);
+	HeadMaterial->SetVectorParameterValue("HeadColor", InColor);
+	FaceMaterial->SetVectorParameterValue("FaceColor", InColor);
+}
+
+void ACPlayer_Swat::OnAim()
+{
+	CheckTrue(Action->IsUnarmedMode());
+	CheckTrue(State->IsEquipMode());
+
+	bUseControllerRotationYaw = true;
+	GetCharacterMovement()->bOrientRotationToMovement = false;
+
+	SpringArm->TargetArmLength = 100;
+	SpringArm->SocketOffset = FVector(0, 60, 10);
+
+	OnZoomIn();
+	//Camera->FieldOfView = 40;
+
+	Rifle = Cast<ACDoAction_Rifle>(Action->GetCurrent()->GetDoAction());
+
+	Rifle->Begin_Aiming();
+
+	Crosshair->SetVisibility(ESlateVisibility::Visible);
+}
+
+void ACPlayer_Swat::OffAim()
+{
+	CheckTrue(Action->IsUnarmedMode());
+	CheckTrue(State->IsEquipMode());
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	SpringArm->TargetArmLength = 200;
+	SpringArm->SocketOffset = FVector(0, 120, 0);
+
+	OnZoomOut();
+	//Camera->FieldOfView = 90;
+
+	Rifle = Cast<ACDoAction_Rifle>(Action->GetCurrent()->GetDoAction());
+
+	Rifle->End_Aiming();
+
+	Crosshair->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void ACPlayer_Swat::OnFocus()
+{
+	Crosshair->OnFocus();
+}
+
+void ACPlayer_Swat::OffFocus()
+{
+	Crosshair->OffFocus();
+}
+
+void ACPlayer_Swat::OffFire()
+{
+	if (Rifle != nullptr)
+	{
+		Rifle->End_Fire();
+	}
+}
---
+++ C++/ActionGame/Source/ActionGame/Characters/CPlayer_Swat.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actions/IRifle.h"
+#include "Characters/CPlayer.h"
+#include "CPlayer_Swat.generated.h"
+
+UCLASS()
+class ACTIONGAME_API ACPlayer_Swat : public ACPlayer, public IIRifle
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Widgets")
+		TSubclassOf<class UCUserWidget_Crosshair> CrosshairClass;
+
+private:
+	UFUNCTION()
+		void OnRifle();
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+		void OnZoomIn();
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void OnZoomOut();
+
+public:
+	FORCEINLINE class ACDoAction_Rifle* GetRifle() { return Rifle; }
+	FORCEINLINE virtual bool IsAiming() { return bAiming; }
+	
+public:
+	ACPlayer_Swat();
+
+public:
+	void GetLocationAndDirection(FVector& OutStart, FVector& OutEnd, FVector& OutDirection) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	// 입력을 받아 원하는대로 처리할 수 있도록 해주는 함수
+	void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	void OnFocus() override;
+	void OffFocus() override;
+
+private:
+	// 액션 입력은 parameter가 없다
+	void OnRunning();
+	void OffRunning();
+
+	// Aim mode
+	void OnAim();
+	void OffAim();
+
+	void OffFire();
+
+public:
+	virtual void ChangeColor(FLinearColor InColor) override;
+
+private:
+	class UMaterialInstanceDynamic* BodyMaterial;
+	class UMaterialInstanceDynamic* HeadMaterial;
+	class UMaterialInstanceDynamic* FaceMaterial;
+
+	class UCUserWidget_Crosshair* Crosshair;
+
+	class ACDoAction_Rifle* Rifle;
+	bool bAiming;
+
+};
